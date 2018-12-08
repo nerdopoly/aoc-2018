f8dcd2cadd25fadab6ca857cc9d6f1a453db7bf0
Adam Birtles <nerdopoly@icloud.com>
2018-12-08 22:57:45 +0000

    Clean up day 7 a little

--- 07-the-sum-of-its-parts/part2.hs
+++ 07-the-sum-of-its-parts/part2.hs
@@ -15,25 +15,17 @@ fill size src dest
     | otherwise = dest
     where diff = size - length dest
 
-workers :: Int
-workers = 5
-
 solve :: Requirements -> Int
 solve = go 0 []
     where
         go :: Int -> [(Step,Int)] -> Requirements -> Int
         go t jobs rs
-            | completed     = t
-            | jobsAvailable = go t (fill workers a jobs) rs
-            | jobsDone      = go t (jobs \\ done) (removeAll rs . map fst $ done)
-            | otherwise     = go (t + 1) (tick jobs) rs
-            where a = map init $ (available rs) \\ (map fst jobs)
-                  init s = (s,time s)
-                  tick = map (\(s,r) -> (s,r - 1))
+            | null rs && null jobs            = t
+            | length jobs < 5 && not (null a) = go t (fill 5 a jobs) rs
+            | not (null done)                 = go t (jobs \\ done) (removeAll rs . map fst $ done)
+            | otherwise                       = go (t + 1) (map (\(s,r) -> (s,r - 1)) jobs) rs
+            where a = map (\step -> (step,time step)) $ (available rs) \\ (map fst jobs)
                   done = filter ((<1) . snd) $ jobs
-                  completed = (null rs && null jobs)
-                  jobsAvailable = length jobs < workers && not (null a)
-                  jobsDone = not . null $ done
 
 main = do
     input <- readFile "input.txt"
